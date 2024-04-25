eea03390f7399dc3f76fbc6c2fa62a34a7efb9b6
Devin Khun <lightningowl2004@gmail.com>
2024-04-25 15:20:01 -0700

    feat: Posts Gallery and Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,60 @@
-const Post = () => {
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { supabase } from "../../client";
+
+const Post = (props) => {
+    const [upvotes, setUpvotes] = useState(props.upvotes);
+    const [timePosted, setTimePosted] = useState("");
+
+    useEffect(() => {
+        const getTimeDifference = (createdAt) => {
+            const currentTime = new Date();
+            const postedTime = new Date(createdAt);
+            const difference = currentTime - postedTime;
+            const seconds = Math.floor(difference / 1000);
+            const minutes = Math.floor(seconds / 60);
+            const hours = Math.floor(minutes / 60);
+            const days = Math.floor(hours / 24);
+            const weeks = Math.floor(days / 7);
+
+            if (weeks > 0) {
+                setTimePosted(`Posted ${weeks} ${weeks === 1 ? "week" : "weeks"} ago`);
+            } else if (days > 0) {
+                setTimePosted(`Posted ${days} ${days === 1 ? "day" : "days"} ago`);
+            } else if (hours > 0) {
+                setTimePosted(`Posted ${hours} ${hours === 1 ? "hour" : "hours"} ago`);
+            } else if (minutes > 0) {
+                setTimePosted(`Posted ${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`);
+            } else {
+                setTimePosted("Posted just now");
+            }
+        };
+        getTimeDifference(new Date(props.createdAt));
+    }, [props.createdAt])
+
+    const upvote = async (event) => {
+        setUpvotes(upvotes + 1);
+        event.preventDefault();
+        await supabase
+            .from("Posts")
+            .update({upvotes: upvotes + 1})
+            .eq("id", props.id);
+        window.location = "/gallery";
+    };
+    
     return (
-        <div>
-            <h1>Post</h1>
+        <div className="post">
+            <Link to={`/info/${props.id}`} key={props.id} className="post-link">
+                <div className="post-container">
+                    <h3>{props.title}</h3>
+                    <p>{props.content}</p>
+                    <h5>{props.upvotes} upvotes</h5>
+                    <img className="post-img" src={props.imageURL} />
+                    <p>{timePosted}</p>
+                </div>
+                <Link to={`/edit/${props.id}`}><button className="post-link-button">Edit Post</button></Link>
+                <input className="upvote-button" type="submit" value="Upvote 👍" onClick={upvote} />
+            </Link>
         </div>
     );
 };
--- src/pages/ReadPosts.jsx
+++ src/pages/ReadPosts.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../../client";
+import Post from "../components/Post";
+
 const ReadPosts = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        // Read all posts from the database table
+        const fetchPosts = async () => {
+            const {data} = await supabase
+                .from("Posts")
+                .select()
+                .order("created_at", {ascending: true});
+            setPosts(data);
+        }
+        fetchPosts();
+    }, []);
+
     return (
-        <div>
-            <h1>Read Posts</h1>
+        <div className="read-posts">
+            <h1 className="read-posts-title">Forum Posts</h1>
+            <div className="read-posts-gallery">
+                {
+                    posts && posts.length > 0 ?
+                    posts.map((post, index) =>
+                        <Post key={index} id={post.id} title={post.title} content={post.content} imageURL={post.image_url} upvotes={post.upvotes} secretKey={post.secret_key} createdAt={post.created_at} />
+                    ) : <h2>{'No Posts Yet 😞'}</h2>
+                }
+            </div>
         </div>
     );
 };
