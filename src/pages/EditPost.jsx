298f018b153c162d26f0f4c410ffa2f2b784f26a
Devin Khun <lightningowl2004@gmail.com>
2024-04-24 22:49:18 -0700

    feat: Update Post

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,7 +1,80 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { supabase } from "../../client";
+
 const EditPost = () => {
+    const {id} = useParams();
+    const [post, setPost] = useState({id: null, title: "", content: "", imageUrl: "", upvotes: 0, secretKey: ""});
+
+    useEffect(() => {
+        const grabCurrentInfo = async (event) => {
+            event.preventDefault();
+            const {data, error} = await supabase
+                .from("Posts")
+                .select()
+                .eq("id", id)
+                .single();
+            if (error) {
+                throw error;
+            }
+            if (data) {
+                setPost(data);
+            }
+        };
+        grabCurrentInfo();
+    }, [id]);
+
+    const updatePost = async (event) => {
+        event.preventDefault();
+        await supabase
+            .from("Posts")
+            .update({title: post.title, content: post.content, image_url: post.imageUrl, upvotes: post.upvotes, secret_key: post.secretKey})
+            .eq("id", id);
+        window.location = "/gallery";
+    };
+
+    const deletePost = async (event) => {
+        event.preventDefault();
+        await supabase
+            .from("Posts")
+            .delete()
+            .eq("id", id);
+        window.location = "/gallery";
+    };
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setPost((prev) => {
+            return {
+                ...prev,
+                [name]: value,
+            };
+        });
+    };
+
     return (
-        <div>
-            <h1>Edit Post</h1>
+        <div className="edit-post">
+            <h1>Update Post</h1>
+            <form>
+                <label>Title</label> <br />
+                <input type="text" id="title" name="title" value={post.title} placeholder="Title" onChange={handleChange} /> <br />
+                <br />
+
+                <label>Content (Optional)</label> <br />
+                <textarea rows="8" type="text" id="content" name="content" value={post.content} placeholder="Content (Optional)" onChange={handleChange} /> <br />
+                <br />
+
+                <label>Image URL (Optional)</label> <br />
+                <input type="text" id="image-url" name="imageUrl" value={post.image_url} placeholder="Image URL (Optional)" onChange={handleChange} /> <br />
+                <br />
+
+                <label>Secret Key (Optional)</label> <br />
+                <input type="text" id="secret-key" name="secretKey" value={post.secret_key} placeholder="Secret Key (Optional)" onChange={handleChange} /> <br />
+                <br />
+                
+                <input className="update-button" type="submit" value="Update Post" onClick={updatePost} />
+                <input className="delete-button" type="submit" value="Delete Delete" onClick={deletePost} />
+            </form>
         </div>
     );
 };
