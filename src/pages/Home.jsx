2ff4b842aa10ddf5a6a6279936da87a16fb66522
Devin Khun <lightningowl2004@gmail.com>
2024-04-26 23:45:51 -0700

    feat: Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import AugustaImg from "../assets/augusta.jpeg";
+
 const Home = () => {
     return (
         <div className="home-container">
-            <h1>Home</h1>
+            <h1>Welcome to GolfGather Hub! ⛳</h1>
+            <h3>Join the ultimate community of golf enthusiasts!</h3>
+            <img className="home-img" src={AugustaImg}/>
+            <Link to="/gallery"><button className="explore-btn">Explore Now</button></Link>
         </div>
     );
 };
